yokai-php/batch-doctrine-orm

Packages tests (#10)
* Moved unit tests to their respective packages

* Added dev autoload for tests in every package

* Normalize .gitignore files

* Add tests dir to git export-ignore

* Updated tests configuration to be able to run all tests

* Add tests config for each package

* Updated to PhpUnit 9.4

tests/User.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yokai\Batch\Tests\Bridge\Doctrine\ORM;
+
+class User
+{
+    public $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+}

yokai-php/batch-doctrine-persistence

tests/User.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yokai\Batch\Tests\Bridge\Doctrine\Persistence;
+
+class User
+{
+    public $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+}